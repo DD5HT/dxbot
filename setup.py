1c74dc996a540c2dd96dc390dc02c941837102af
DD5HT <bartuni38@googlemail.com>
2017-08-04 16:38:47 +0200

    git

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dxbot',
+      version='0.1.1',
+      description='',
+      author='',
+      author_email='',
+      url='',
+      packages=['distutils', 'distutils.command'],
+     )
+     
